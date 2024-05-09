9467f0de62fd8b3b76da5b0693e64f3a092ea3de
Veronica Parra <vero.parragraca@gmail.com>
2024-05-09 11:47:13 +0200

    add/added jest .eslint

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -1,15 +1,15 @@
 const { sum, mul, sub, div } = require('../src/math');
 
-test('Adding 1 + 1 equals 2', () => {
+test('Adding 1 + 1 equals 2', (aa) => {
   expect(sum(1, 1)).toBe(2);
 });
-test('Multiplying 1 * 1 equals 1', () => {
+test('Multiplying 1 * 1 equals 1', (12) => {
   expect(mul(1, 1)).toBe(1);
 });
 test('Subtracting 1 - 1 equals 0', () => {
   expect(sub(1, 1)).toBe(0);
 });
-test('Dividing 1 / 1 equals 1', () => {
+test('Dividing 1 / 1 equals 1', (15) => {
   expect(div(1, 1)).toBe(1);
 });
 test('Dividing 1/0 equals infinite',()=>{
